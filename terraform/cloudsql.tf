e2c7e6ada388a09ee322c040683840d3dd00e4fc
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-22 14:59:30 +0530

    Update cloudsql.tf

--- terraform/cloudsql.tf
+++ terraform/cloudsql.tf
@@ -1,52 +1,36 @@
-########################################
-# Cloud SQL Postgres for Bindplane
-########################################
+resource "random_id" "suffix" {
+  byte_length = 3
+}
 
 resource "google_sql_database_instance" "bindplane_postgres" {
-  name             = "bindplane-db"
+  name             = "bindplane-db-${random_id.suffix.hex}"
   database_version = "POSTGRES_15"
   region           = var.region
 
   settings {
-    tier = "db-f1-micro" # adjust based on load
+    tier = "db-f1-micro"
+    disk_size = 50
 
     ip_configuration {
-      ipv4_enabled = true
+      ipv4_enabled    = true
       authorized_networks {
-        value = "0.0.0.0/0"  # WARNING: allow all, restrict later
+        value = "0.0.0.0/0"  # restrict later
       }
+    }
+
+    backup_configuration {
+      enabled = true
     }
   }
 }
 
-# Create the database
 resource "google_sql_database" "bindplane" {
   name     = "bindplane"
   instance = google_sql_database_instance.bindplane_postgres.name
 }
 
-# Create the user
 resource "google_sql_user" "bindplane_user" {
   name     = var.db_user
   instance = google_sql_database_instance.bindplane_postgres.name
   password = var.db_password
-}
-
-# Set schema privileges (Terraform provider doesn't support ALTER SCHEMA directly)
-resource "null_resource" "bindplane_schema_grants" {
-  depends_on = [
-    google_sql_user.bindplane_user,
-    google_sql_database.bindplane
-  ]
-
-  provisioner "local-exec" {
-    command = <<EOT
-PGPASSWORD=${var.db_password} psql \
--h ${google_sql_database_instance.bindplane_postgres.public_ip_address} \
--U ${var.db_user} \
--d ${google_sql_database.bindplane.name} \
--c "GRANT USAGE, CREATE ON SCHEMA public TO ${var.db_user};" \
--c "ALTER SCHEMA public OWNER TO ${var.db_user};"
-EOT
-  }
 }
