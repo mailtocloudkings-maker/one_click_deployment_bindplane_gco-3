29dd41af46c4ed2e8d55f709e5cb439db0d3271a
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-22 17:50:41 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,54 +1,40 @@
 variable "project_id" {
-  description = "GCP Project ID"
-  type        = string
+  type = string
 }
 
 variable "region" {
-  description = "GCP Region"
-  type        = string
+  type = string
 }
 
 variable "zone" {
-  description = "GCP zone for VM"
-  type        = string
-  default     = "us-central1-a"
+  type    = string
+  default = "us-central1-a"
 }
 
 variable "alert_email" {
-  description = "Email for alert notifications"
-  type        = string
+  type = string
 }
 
 variable "bindplane_api_key" {
-  description = "BindPlane API key"
-  type        = string
-  sensitive   = true
+  type      = string
+  sensitive = true
 }
 
 variable "db_user" {
-  description = "Bindplane Postgres user"
-  type        = string
+  type = string
 }
 
 variable "db_password" {
-  description = "Bindplane Postgres password"
-  type        = string
-  sensitive   = true
-}
-
-variable "bindplane_admin_user" {
-  description = "Bindplane admin username"
-  type        = string
+  type      = string
+  sensitive = true
 }
 
 variable "bindplane_admin_password" {
-  description = "Bindplane admin password"
-  type        = string
-  sensitive   = true
+  type      = string
+  sensitive = true
 }
 
 variable "bindplane_license" {
-  description = "Bindplane license key"
-  type        = string
-  sensitive   = true
+  type      = string
+  sensitive = true
 }
