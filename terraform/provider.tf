5549c76e67c3b7eb30aaeb8c72f944a4ff6d1915
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-22 14:58:29 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,17 +1,20 @@
 terraform {
-  required_version = ">= 1.5"
+  required_version = ">= 1.4.0"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-    bindplane = {
-      source = "observiq/bindplane"
+      version = ">= 5.0"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
+      version = ">= 3.5"
     }
+  }
+
+  backend "gcs" {
+    bucket = "my-terraform-state"   # store your tf state securely
+    prefix = "bindplane"
   }
 }
 
